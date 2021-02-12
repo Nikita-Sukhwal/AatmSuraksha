232e84bb84beb7d62bb1bccdcb117f26e457f8e3
Nikita Sukhwal <17cse067@gweca.ac.in>
2021-02-12 23:14:05 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 Email="helpme.suraksha@gmail.com"
 Password="WomenSafety1"
 SENDGRID_API_KEY='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'
-CALL_API='LRl7M4jmgdYDgqwoIgrUEDgJE'
+CALL_API='7u0Lw5XJoLgK0kLGMpnBnaGND'
 Helpline_no="+918949303552"
