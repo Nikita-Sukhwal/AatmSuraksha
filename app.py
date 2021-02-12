5f168abd56ed83907062ddb038106722336bd4e9
Nikita Sukhwal <17cse067@gweca.ac.in>
2021-02-12 23:30:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def emergency():
         list_phones = email_object["user_phones"].split(',')
 
         for contacts in list_phones:
-        	try:
+		try:
         		message = "I am "+name+' It is Emergency, Help immediately '+" check my location- "+link
         		msg = client.message_create(name,contacts,message)
         		print(msg.__dict__)
@@ -170,13 +170,6 @@ def emergency():
         		for error in e.errors:
         			print(error)
 				
-	try:
-		message = "I am "+name+' It is Emergency, Help immediately '+" check my location- "+link
-		msg = client.message_create(name,"+918441025919",message)
-		print(msg.__dict__)
-	except messagebird.client.ErrorException as e:
-		for error in e.errors:
-			print(error)
 	#SEE YOUR OWN LOCATION
 	return render_template('Location.html')
 
